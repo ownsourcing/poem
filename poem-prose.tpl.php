83f60cb83ec078913aae1373159000d826e44587
Ben Bunk <benjamin.bunk@acquia.com>
2012-11-14 21:58:32 -0500

    Added poem-prose.tpl.php, removed theme_poem_prose().

---
+++ poem-prose.tpl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file poem-prose.tpl.php
+ */
+
+?>
+
+<h1> <?php print t("Rose are $roses"); ?> </h1>
+<p>&nbsp;&nbsp;&nbsp;&nbsp; <?php print t("Rose are $roses"); ?> </p>
+<p>&nbsp;&nbsp;&nbsp;&nbsp; <?php print t("Violets are $violets"); ?>  </p>
+<p>&nbsp;&nbsp;&nbsp;&nbsp; <?php print t("Nobody loves Form API"); ?> </p>
+<p>&nbsp;&nbsp;&nbsp;&nbsp; More than I do </p>
